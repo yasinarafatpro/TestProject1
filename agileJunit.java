2299b1f7e6e469a0b99bdb85999a72efb3611b34
yasinarafatpro <51745256+yasinarafatpro@users.noreply.github.com>
2020-08-04 23:31:12 +0600

    Add files via upload

---
+++ agileJunit.java
@@ -0,0 +1,11 @@
+
+package junitTest;
+
+
+public class agileJunit {
+    public int Add(String s1,String s2){
+        int int1=Integer.parseInt(s1);
+        int int2=Integer.parseInt(s2);
+        return int1+int2;
+    }
+}
